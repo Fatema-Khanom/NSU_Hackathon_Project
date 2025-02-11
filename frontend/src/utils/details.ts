e00418804c266fe95a284e495fb943b5999e4f42
Fatema Khanom <fatemaurmi2019@gmail.com>
2025-02-11 22:21:50 +0600

    create frontend folder

---
+++ frontend/src/utils/details.ts
@@ -0,0 +1,28 @@
+export interface LoginData {
+    email: string;
+    password: string;
+}
+
+export interface RegisterData {
+    firstName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface OtpData {
+    email: string;
+    otp: string;
+}
+
+export interface CrimeData {
+    title: string;
+    description: string;
+    location: string;
+    date: string;
+}
+
+export interface CommentData {
+    crimeId: string;
+    comment: string;
+}
